1fff194a995fc07880826a0e8fb747fcf2df9eed
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-09-09 13:20:09 -0500

    added logo to navbar

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,16 @@ module.exports = {
                 }
             },
             {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 25000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    },
+                }]
+            },
+            {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             }
