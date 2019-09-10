7047117df36b823dccc2e1d37d9b6b5a5d89f627
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-09-09 22:20:13 -0500

    updated content of About Me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,15 +17,22 @@ const AboutMe = (props) => {
                 <Col lg={8} className="story-container">
                     <h3 className="about-header">About Me</h3>
                     <div className="about-story">
-                        <p>This is where I will need to write the story about who I am and
-                        how I became a software engineer.  Hopefully this is interesting.
+                        <p>My name is David and I am a recent transplant to Austin, TX from New York, NY.  I
+                            am a full-stack software engineer that is passionate about UI design and developing
+                            visually appealing applications.  This website contains a portfolio of my work, a
+                            blog about my current events, and also links to connect on social media.
+                        </p>Software development has become my passion and I live to code.  I find pleasure in building
+                        stunning websites from a blank page and I enjoy the creativity involved in web design.  I have
+                        worked on small teams of four to six developers to create production level applications of which
+                        I have screenshots below.  I never thought that I would find my true calling when it comes to a
+                        career, but now I know that I have found as a software engineer.
+                        <p>
                         </p>
-                        <p>I just want to check out how doing multiple paragraphs will look on the
-                            screen.  Just testing things out to see if I can make this look great.
-                        </p>
-                        <p>In addition to talking about my past, I can talk about my life in New York
-                            and why I prefer Austin.  Also, discussing a career change later in life
-                            could be inspiring to others who are scared to make the change.
+                        <p>I have been in Austin for a little under a year and am enjoying everything that the city
+                            has to offer.  There is a fantastic food scene here, plenty of outdoor activities to enjoy,
+                            and the culture is unbeatable.  My summer has ended with a trip to Hamilton pool, which is
+                            absolutely stunning, as well as a day trip on Lake Travis enjoying the sun with some good
+                            friends.
                         </p>
                     </div>
                 </Col>
