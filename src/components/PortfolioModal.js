427d1fb931e9efdb16dbe82bf8ccdc4f5111a500
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-09-11 15:20:24 -0500

    completed content for Portfolio Modal

--- src/components/PortfolioModal.js
+++ src/components/PortfolioModal.js
@@ -31,15 +31,33 @@ const PortfolioModal = (props) => {
                             <img className="modal-image" src={blog} alt="DCG blog" />
                         </Col>
                         <Col>
-                            <div className="modal-description"></div>
+                            <div className="modal-description">This is my portfolio page displaying some of
+                            my previous work.  In itself this page is an application as it has taken many skills
+                            to build.  I started with a blank page and have ended up with a beautiful site.  I
+                            designed logos and layout entirely from scratch.  I built my own Webpack and used
+                            many Bootstrap components to help out with the functionality of the site.  There are
+                            two final pieces that I still need to complete.  First, I would like to switch the photos
+                            of each application over to videos.  Second, I need to get this portfolio deployed for
+                            the world to see!  (If you are viewing this online right now, it means that I was able to
+                            accomplish my second goal).
+                            </div>
                         </Col>
                     </Row>
                 </Container>
             </Modal.Body>
             <Modal.Footer>
-                <Button className="modal-button" variant="dark" onClick={props.handleClose}>
-                    Close
+                <Container>
+                    <Row>
+                        <Col xs={9} className="tech">
+                            <div><strong>Technologies Used: </strong>React, Bootstrap, Express, Digital Ocean, VIM, NGINX, Webpack, Babel</div>
+                        </Col>
+                        <Col className="modal-close">
+                            <Button className="modal-button" variant="dark" onClick={props.handleClose}>
+                                Close
                 </Button>
+                        </Col>
+                    </Row>
+                </Container>
             </Modal.Footer>
         </Modal>
     );
