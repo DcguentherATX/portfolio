525f873a597319da8429fb03a3227026beefa08a
DAVID GUENTHER <davidcarlguenther@gmail.com>
2019-09-12 14:53:35 -0500

    added a gulpfile, updated profile pic resizing, and edited about content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,7 +13,9 @@ const AboutMe = (props) => {
         <Container className="about-container">
             <Row>
                 <Col md={12} lg={4} xl={3} className="pic-container">
-                    <img src={profilePic} alt="David Carl Guenther" className="profile-pic" id="about-me"></img>
+                    <div className="inner-pic-container">
+                        <img src={profilePic} alt="David Carl Guenther" className="profile-pic" id="about-me"></img>
+                    </div>
                 </Col>
                 <Col md={12} lg={8} xl={7} className="story-container">
                     {/* <h3 className="about-header">About Me</h3> */}
@@ -22,18 +24,18 @@ const AboutMe = (props) => {
                             am a full-stack software engineer that is passionate about UI design and developing
                             visually appealing applications.  This website contains a portfolio of my work, a
                             blog about my current events, and also links to connect on social media.
-                        </p>Software development has become my passion and I live to code.  I find pleasure in building
-                        stunning websites from a blank page and I enjoy the creativity involved in web design.  I have
-                        worked on small teams of four to six developers to create production level applications of which
-                        I have screenshots below.  I never thought that I would find my true calling when it comes to a
-                        career, but now I know that I have found as a software engineer.
-                        <p>
+                        </p>
+                        <p>Software development has become my passion and I live to code.  I find pleasure in building
+                    stunning websites from a blank page and I enjoy the creativity involved in web design.  I have
+                    worked on small teams of four to six developers to create production level applications which are
+                    featured below.  I never thought that I would find my true calling when it comes to a
+                    career, but now I know that I have found it as a software engineer.
                         </p>
                         <p>I have been in Austin for a little under a year and am enjoying everything that the city
                             has to offer.  There is a fantastic food scene here, plenty of outdoor activities to enjoy,
-                            and the culture is unbeatable.  My summer has ended with a trip to Hamilton pool, which is
-                            absolutely stunning, as well as a day trip on Lake Travis enjoying the sun with some good
-                            friends.
+                            and the culture is unbeatable.  My summer has ended with a trip to Hamilton Pool, which is
+                            absolutely stunning, as well as a day trip on Lake Travis during which I enjoyed the sun with
+                            some good friends.
                         </p>
                     </div>
                 </Col>
@@ -41,7 +43,7 @@ const AboutMe = (props) => {
                     <img lg={12} xl={2} className="about-logo d-none d-xl-block" src={aboutLogo} alt="about-logo" />
                 </Col>
             </Row>
-        </Container>
+        </Container >
     )
 }
 
